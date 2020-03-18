1785d740283253b219eb395779f6a7c790d2ab3d
buldar <buldar666@gmail.com>
2020-03-18 17:48:37 +0300

    start use reducers. refactoring

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import store from "./redux/store";
+import store from "./redux/redux-store";
 import ReactDOM from 'react-dom';
 import App from './App';
 
 
 let rerenderTree = (state) => {
+    debugger;
     ReactDOM.render(<App appState={state}
                          store={store}
                          dispatch={store.dispatch.bind(store)} />, document.getElementById('root'));
@@ -14,7 +15,10 @@ let rerenderTree = (state) => {
 
 rerenderTree(store.getState());
 
-store.subscribe(rerenderTree);
+store.subscribe(() => {
+    let state = store.getState();
+    rerenderTree(state);
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,8 +1,17 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
+let initialState = {
+    posts: [
+        {id: 1, message: 'Hey, Salaga!', likesCount: 23},
+        {id: 2, message: 'Omg! Look like one more...', likesCount: 31},
+        {id: 3, message: 'So sad...', likesCount: 6},
+        {id: 4, message: 'TEST ', likesCount: 3},
+    ],
+    newPostText: '',
+}
 
-export const profileReducer = (state, action) => {
+const profileReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case ADD_POST:
